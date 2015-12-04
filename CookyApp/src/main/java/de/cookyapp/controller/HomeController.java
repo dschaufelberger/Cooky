69b93a73db95814e493dcf4d58e033cf478a804f
Dominik Schaufelberger <dominik.schaufelberger@web.de>
2015-12-04 17:50:32 +0100

    merged feature_LogInOut

--- CookyApp/src/main/java/de/cookyapp/controller/HomeController.java
+++ CookyApp/src/main/java/de/cookyapp/controller/HomeController.java
@@ -3,9 +3,6 @@ package de.cookyapp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-
-import de.cookyapp.viewmodel.authentication.LoginCredentials;
 
 /**
  * Created by Dominik Schaufelberger on 01.12.2015.
--- CookyApp/src/main/java/de/cookyapp/controller/LoginController.java
+++ CookyApp/src/main/java/de/cookyapp/controller/LoginController.java
@@ -1,27 +1,19 @@
 package de.cookyapp.controller;
 
-import javax.validation.Valid;
-
-import de.cookyapp.viewmodel.authentication.LoginCredentials;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by Dominik Schaufelberger on 29.11.2015.
  */
 @Controller
 public class LoginController {
-    @RequestMapping("/loginPage")
+    @RequestMapping( "/loginPage" )
     public String loginPage() {
         return "authentication/LoginPage";
     }
 
-    @RequestMapping("/loginError")
+    @RequestMapping( "/loginError" )
     public String loginError() {
         return "authentication/LoginError";
     }
