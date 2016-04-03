a93a9958e58249d7d777440ad665fdf80e937607
Dominik Schaufelberger <dominik.schaufelberger@web.de>
2016-04-03 20:27:38 +0200

    code format

--- CookyApp/src/main/java/de/cookyapp/persistence/repositories/IBaseCrudRepository.java
+++ CookyApp/src/main/java/de/cookyapp/persistence/repositories/IBaseCrudRepository.java
@@ -10,12 +10,12 @@ import org.springframework.data.repository.Repository;
  * Created by Dominik Schaufelberger on 03.04.2016.
  */
 @NoRepositoryBean
-public interface IBaseCrudRepository<T,ID extends Serializable> extends Repository<T, ID> {
-    void delete(T deleted);
+public interface IBaseCrudRepository <T, ID extends Serializable> extends Repository<T, ID> {
+    void delete( T deleted );
 
     List<T> findAll();
 
-    T findOne(ID id);
+    T findOne( ID id );
 
-    T save(T persisted);
+    T save( T persisted );
 }
--- CookyApp/src/main/java/de/cookyapp/persistence/repositories/IUserCrudRepository.java
+++ CookyApp/src/main/java/de/cookyapp/persistence/repositories/IUserCrudRepository.java
@@ -1,11 +1,10 @@
 package de.cookyapp.persistence.repositories;
 
 import de.cookyapp.persistence.entities.UserEntity;
-import org.springframework.data.repository.CrudRepository;
 
 /**
  * Created by Dominik Schaufelberger on 03.04.2016.
  */
 public interface IUserCrudRepository extends IBaseCrudRepository<UserEntity, Integer> {
-    UserEntity findByUsername(String username);
+    UserEntity findByUsername( String username );
 }
