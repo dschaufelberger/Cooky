954a274d76a6b2baeecb7e0fa15796bfb68bdf54
Dominik Schaufelberger <dominik.schaufelberger@web.de>
2016-04-03 20:25:21 +0200

    code format

--- CookyApp/src/main/java/de/cookyapp/enums/RecipeDifficulty.java
+++ CookyApp/src/main/java/de/cookyapp/enums/RecipeDifficulty.java
@@ -4,11 +4,11 @@ package de.cookyapp.enums;
  * Created by Dominik Schaufelberger on 27.11.2015.
  */
 public enum RecipeDifficulty {
-    EASY("Easy"), MEDIUM("Medium"), ADVANCED("Advanced"), EXPERT("Expert");
+    EASY( "Easy" ), MEDIUM( "Medium" ), ADVANCED( "Advanced" ), EXPERT( "Expert" );
 
     private String recipeDifficulty;
 
-    RecipeDifficulty(String recipeDifficulty) {
+    RecipeDifficulty( String recipeDifficulty ) {
         this.recipeDifficulty = recipeDifficulty;
     }
 
