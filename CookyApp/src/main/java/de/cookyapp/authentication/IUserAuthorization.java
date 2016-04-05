bf475a777733b90ef403cf3580613aec347d7617
Dominik <dominik.schaufelberger>
2016-04-05 20:03:59 +0200

    COOK-80 - implement uc:log in to account
    ADD: implemented part of the user crud service
    ADD: spring bean for user authorization

---
+++ CookyApp/src/main/java/de/cookyapp/authentication/IUserAuthorization.java
@@ -0,0 +1,12 @@
+package de.cookyapp.authentication;
+
+import org.springframework.security.core.Authentication;
+
+/**
+ * Created by Dominik Schaufelberger on 05.04.2016.
+ */
+public interface IUserAuthorization {
+    boolean hasAllAuthorities( Authentication authentication, String[] authorities);
+    boolean hasAnyAuthority(Authentication authentication, String[] authorities);
+    boolean hasAuthority(Authentication authentication, String authority);
+}
