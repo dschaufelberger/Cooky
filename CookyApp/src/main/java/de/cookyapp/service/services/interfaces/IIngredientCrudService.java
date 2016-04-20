9e79d9349b62bb15ee4194a99f59c46c97c3fec6
Dominik Schaufelberger <dominik.schaufelberger@web.de>
2016-04-20 16:56:51 +0200

    COOK-119 - refactor data access
    REFACTOR: code format

--- CookyApp/src/main/java/de/cookyapp/service/services/interfaces/IIngredientCrudService.java
+++ CookyApp/src/main/java/de/cookyapp/service/services/interfaces/IIngredientCrudService.java
@@ -1,17 +1,17 @@
 package de.cookyapp.service.services.interfaces;
 
-import de.cookyapp.service.dto.Ingredient;
-
 import java.util.List;
+
+import de.cookyapp.service.dto.Ingredient;
 
 /**
  * Created by Jasper on 12.04.2016.
  */
 public interface IIngredientCrudService {
 
-    void save (List<Ingredient> ingredients);
+    void save( List<Ingredient> ingredients );
 
-    void saveRecipeIngredient (int recipeId, List<Ingredient> ingredients);
+    void saveRecipeIngredient( int recipeId, List<Ingredient> ingredients );
 
-    List<Ingredient> loadRecipeIngredients(int recipeId);
+    List<Ingredient> loadRecipeIngredients( int recipeId );
 }
