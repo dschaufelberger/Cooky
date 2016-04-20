2f3f2c623ea328062662cf72fb255e0eb43c2e9a
Dominik Schaufelberger <dominik.schaufelberger@web.de>
2016-04-20 17:18:53 +0200

    COOK-119 - refactor data access
    CHANGE: changed the thrown exceptions to java internal ones.

--- CookyApp/src/main/java/de/cookyapp/service/services/AddressService.java
+++ CookyApp/src/main/java/de/cookyapp/service/services/AddressService.java
@@ -4,7 +4,6 @@ import de.cookyapp.service.dto.Address;
 import de.cookyapp.service.services.interfaces.IAddressService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 /**
  * Created by Dominik Schaufelberger on 09.04.2016.
@@ -14,26 +13,26 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 public class AddressService implements IAddressService {
     @Override
     public void createAddressForUser( int userID, Address address ) {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public void removeAddressFromUser( int userID, int addressID ) {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public void updateAddress( Address address ) {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public Address getAddress( int addressID ) {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public Address getAddressForUser( int userID ) {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException();
     }
 }
