d81bafaecfa2ab31983d6237e09be745769088cc
Dominik <dominik.schaufelberger>
2016-04-25 11:04:32 +0200

    COOK-134 - HW: Testing (Details in description) ADD: created test class for the recipe crud service

---
+++ CookyApp/src/test/java/de/cookyapp/service/IRecipeCrudServiceTest.java
@@ -0,0 +1,55 @@
+package de.cookyapp.service;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Dominik Schaufelberger on 25.04.2016.
+ */
+public class IRecipeCrudServiceTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testDeleteRecipe() throws Exception {
+
+    }
+
+    @Test
+    public void testCreateRecipe() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateRecipe() throws Exception {
+
+    }
+
+    @Test
+    public void testGetRecipe() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAllRecipes() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAllRecipesByName() throws Exception {
+
+    }
+
+    @Test
+    public void testSearchRecipesContaining() throws Exception {
+
+    }
+}
