9beb00e14304ed6a5b808f45d9ef20ff4d382cc0
1developer1 <fueranmeldungen@t-online.de>
2016-04-25 21:35:44 +0200

    implemented methods for AddressService

--- CookyApp/src/main/java/de/cookyapp/service/services/AddressService.java
+++ CookyApp/src/main/java/de/cookyapp/service/services/AddressService.java
@@ -1,38 +1,86 @@
 package de.cookyapp.service.services;
 
+import de.cookyapp.persistence.entities.AddressEntity;
+import de.cookyapp.persistence.entities.UserEntity;
+import de.cookyapp.persistence.repositories.IAddressCrudRepository;
+import de.cookyapp.persistence.repositories.IUserCrudRepository;
 import de.cookyapp.service.dto.Address;
+import de.cookyapp.service.dto.User;
 import de.cookyapp.service.services.interfaces.IAddressService;
+import de.cookyapp.service.services.interfaces.IUserCrudService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
- * Created by Dominik Schaufelberger on 09.04.2016.
+ * Created by Mario Kaiser on 25.04.2016.
+ *
  */
 @Transactional
 @Service
 public class AddressService implements IAddressService {
-    @Override
-    public void createAddressForUser( int userID, Address address ) {
-        throw new UnsupportedOperationException();
+    private IAddressCrudRepository addressCrudRepository;
+    private IUserCrudRepository userCrudRepository;
+
+    @Autowired
+    public AddressService(IAddressCrudRepository addressCrudRepository, IUserCrudRepository userCrudRepository) {
+        this.addressCrudRepository = addressCrudRepository;
     }
 
     @Override
-    public void removeAddressFromUser( int userID, int addressID ) {
-        throw new UnsupportedOperationException();
+    public void createAddressForUser(int userID, Address address) {
+        AddressEntity addressEntity = this.addressCrudRepository.findOne(address.getId());
+        addressEntity.setCity(address.getCity());
+        addressEntity.setStreet(address.getStreet());
+        addressEntity.setPostcode(address.getPostcode());
+        addressEntity.setHouseNumber(address.getHouseNumber());
+        this.addressCrudRepository.save(addressEntity);
+
+        UserEntity userEntity = this.userCrudRepository.findOne(userID);
+        userEntity.setAddress(addressEntity);
+        this.userCrudRepository.save(userEntity);
     }
 
     @Override
-    public void updateAddress( Address address ) {
-        throw new UnsupportedOperationException();
+    public void removeAddressFromUser(int userID, int addressID) {
+        AddressEntity addressEntity = this.addressCrudRepository.findOne(addressID);
+        this.addressCrudRepository.delete(addressEntity);
+        UserEntity userEntity = this.userCrudRepository.findOne(userID);
+        userEntity.setAddress(null);
+        this.userCrudRepository.save(userEntity);
+        //TODO: remove address only from User or delete Address?!
     }
 
     @Override
-    public Address getAddress( int addressID ) {
-        throw new UnsupportedOperationException();
+    public void updateAddress(Address address) {
+        AddressEntity addressEntity = this.addressCrudRepository.findOne(address.getId());
+        if (addressEntity != null) {
+            addressEntity.setStreet(address.getStreet());
+            addressEntity.setPostcode(address.getPostcode());
+            addressEntity.setHouseNumber(address.getHouseNumber());
+            addressEntity.setCity(address.getCity());
+            this.addressCrudRepository.save(addressEntity);
+        }
+        //TODO: [1developer1]  implement Exception Handling --> InvalidAddressID ?!
     }
 
     @Override
-    public Address getAddressForUser( int userID ) {
-        throw new UnsupportedOperationException();
+    public Address getAddress(int addressID) {
+        AddressEntity addressEntity = this.addressCrudRepository.findOne(addressID);
+        Address address = this.getAddressIfExistant(addressEntity);
+        return address;
     }
+
+    @Override
+    public Address getAddressForUser(int userID) {
+        UserEntity userEntity = this.userCrudRepository.findOne(userID);
+        AddressEntity addressEntity = userEntity.getAddress();
+        Address address = this.getAddressIfExistant(addressEntity)
+        return address;
+    }
+
+    private Address getAddressIfExistant(AddressEntity addressEntity) {
+        return addressEntity == null ? null : new Address(addressEntity);
+    }
+
 }
