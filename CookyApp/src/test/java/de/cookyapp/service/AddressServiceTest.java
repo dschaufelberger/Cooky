b28fa28862434b0cb001c754b1d6b1070c8fa154
1developer1 <fueranmeldungen@t-online.de>
2016-05-04 14:00:25 +0200

    implemented remaining test cases

--- CookyApp/src/test/java/de/cookyapp/service/AddressServiceTest.java
+++ CookyApp/src/test/java/de/cookyapp/service/AddressServiceTest.java
@@ -1,8 +1,13 @@
 package de.cookyapp.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import de.cookyapp.persistence.entities.AddressEntity;
 import de.cookyapp.persistence.entities.UserEntity;
 import de.cookyapp.service.dto.Address;
 import de.cookyapp.service.mocks.AddressRepositoryMock;
+import de.cookyapp.service.mocks.AuthenticationMock;
 import de.cookyapp.service.mocks.UserRepositoryMock;
 import de.cookyapp.service.services.AddressService;
 import de.cookyapp.service.services.interfaces.IAddressService;
@@ -23,20 +28,21 @@ public class AddressServiceTest {
     private AddressRepositoryMock repositoryMock;
     private IAddressService service;
     private UserRepositoryMock userRepositoryMock;
+    private AuthenticationMock authentication;
+    private UserEntity userDummy;
+    private Address newAddress;
 
 
     @Before
     public void setUp() throws Exception {
         repositoryMock = new AddressRepositoryMock();
         userRepositoryMock = new UserRepositoryMock();
-        this.service = new AddressService( repositoryMock, userRepositoryMock );
-    }
-
-    @Test
-    public void testCreateAddressForUser() throws Exception {
+        //Authentication
+        authentication = new AuthenticationMock( "CookyTester" );
+        this.service = new AddressService( repositoryMock, userRepositoryMock, authentication );
 
         //initialize User
-        UserEntity userDummy = new UserEntity();
+        userDummy = new UserEntity();
         userDummy.setUsername( "CookyTester" );
         userDummy.setId( 1 );
 
@@ -44,39 +50,85 @@ public class AddressServiceTest {
         this.userRepositoryMock.save( userDummy );
 
         //initialize Address
-        Address newAddress = new Address();
+        newAddress = new Address();
         newAddress.setStreet( "Teststrasse" );
         newAddress.setPostcode( "7777" );
         newAddress.setHouseNumber( "9" );
         newAddress.setCity( "Testdorf" );
+    }
 
-
+    @Test
+    public void testCreateAddressForUser() throws Exception {
         //act
         this.service.createAddressForUser( userDummy.getId(), newAddress );
-
         //assert
-        Assert.assertEquals( this.userRepositoryMock.findAll().size(), 1 );
-
+        Assert.assertEquals( 1, this.repositoryMock.getEntities().size() );
     }
 
     @Test
     public void testRemoveAddressFromUser() throws Exception {
+        AddressEntity addressEntity = new AddressEntity();
+        addressEntity.setCity( newAddress.getCity() );
+        addressEntity.setHouseNumber( newAddress.getHouseNumber() );
+        addressEntity.setPostcode( newAddress.getPostcode() );
+        addressEntity.setStreet( newAddress.getStreet() );
+        addressEntity.setId( 1 );
+        //set up list with address entities
+        List<AddressEntity> entityList = new ArrayList<>();
+        entityList.add( addressEntity );
+        this.repositoryMock.setEntities( entityList );
 
+        //act
+        this.service.removeAddressFromUser( userDummy.getId(), 1 );
+        //assert
+        Assert.assertEquals( 0, this.repositoryMock.getEntities().size() );
     }
 
     @Test
     public void updateAddress() throws Exception {
+        AddressEntity addressEntity = new AddressEntity();
+        addressEntity.setCity( newAddress.getCity() );
+        addressEntity.setHouseNumber( newAddress.getHouseNumber() );
+        addressEntity.setPostcode( newAddress.getPostcode() );
+        addressEntity.setStreet( newAddress.getStreet() );
+        addressEntity.setId( 1 );
+        //set up list wth address entities
+        List<AddressEntity> entityList = new ArrayList<>();
+        entityList.add( addressEntity );
+        this.repositoryMock.setEntities( entityList );
+        //set Id for Address DTO
+        newAddress.setId( 1 );
+        //Update fields
+        newAddress.setCity( "Testcity" );
 
-    }
+        userDummy.setAddress( addressEntity );
+        this.userRepositoryMock.save( userDummy );
+        //act
+        this.service.updateAddress( newAddress );
 
-    @Test
-    public void getAddress() throws Exception {
-
+        Assert.assertEquals( "Testcity", this.repositoryMock.getEntities().get( 0 ).getCity() );
     }
 
     @Test
     public void getAddressForUser() throws Exception {
+        AddressEntity addressEntity = new AddressEntity();
+        addressEntity.setCity( newAddress.getCity() );
+        addressEntity.setHouseNumber( newAddress.getHouseNumber() );
+        addressEntity.setPostcode( newAddress.getPostcode() );
+        addressEntity.setStreet( newAddress.getStreet() );
+        addressEntity.setId( 1 );
+        //set up list wth address entities
+        List<AddressEntity> entityList = new ArrayList<>();
+        entityList.add( addressEntity );
+        this.repositoryMock.setEntities( entityList );
+        //set Id for Address DTO
+        newAddress.setId( 1 );
+        // set up user
+        userDummy.setAddress( addressEntity );
+        this.userRepositoryMock.save( userDummy );
 
+        Address address = this.service.getAddressForUser( userDummy.getId() );
+
+        Assert.assertEquals( newAddress.getId(), address.getId() );
     }
-
 }
