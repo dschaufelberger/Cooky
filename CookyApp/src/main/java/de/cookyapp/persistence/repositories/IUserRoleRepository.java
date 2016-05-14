f19a272170d785fd847d3941f66c16c04765ae03
Dominik Schaufelberger <dominik.schaufelberger@web.de>
2016-05-14 22:13:44 +0200

    COOK-151 - Define and implement Cooky user roles
    CHANGE: the repository interface now implements the base Repository<T, ID> from Spring Data with 3 custom methods

--- CookyApp/src/main/java/de/cookyapp/persistence/repositories/IUserRoleRepository.java
+++ CookyApp/src/main/java/de/cookyapp/persistence/repositories/IUserRoleRepository.java
@@ -2,10 +2,15 @@ package de.cookyapp.persistence.repositories;
 
 import de.cookyapp.persistence.entities.UserRoleEntity;
 import de.cookyapp.persistence.entities.UserRoleEntityPK;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.Repository;
 
 /**
  * Created by Dominik Schaufelberger on 14.05.2016.
  */
-public interface IUserRoleRepository extends JpaRepository<UserRoleEntity, UserRoleEntityPK> {
+public interface IUserRoleRepository extends Repository<UserRoleEntity, UserRoleEntityPK> {
+    UserRoleEntity save( UserRoleEntity entity );
+
+    void delete( UserRoleEntity deleted );
+
+    UserRoleEntity findOne( UserRoleEntityPK id );
 }
