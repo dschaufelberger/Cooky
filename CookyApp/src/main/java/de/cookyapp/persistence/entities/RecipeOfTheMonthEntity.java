2713385ad025c4342ff365710eb229d5092c4c61
1developer1 <fueranmeldungen@t-online.de>
2016-05-23 14:50:46 +0200

    Created Entity for RecipeOfTheMonth

---
+++ CookyApp/src/main/java/de/cookyapp/persistence/entities/RecipeOfTheMonthEntity.java
@@ -0,0 +1,86 @@
+package de.cookyapp.persistence.entities;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ * Created by Mario on 23.05.2016.
+ */
+@Entity
+@Table( name = "RecipeOfTheMonth", schema = "Cooky_Dev" )
+public class RecipeOfTheMonthEntity {
+    private int id;
+    private LocalDate updated;
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
+    @Column( name = "ID", nullable = false )
+    public int getId() {
+        return id;
+    }
+
+    public void setId( int id ) {
+        this.id = id;
+    }
+
+
+    @Override
+    public boolean equals( Object o ) {
+        if ( this == o )
+            return true;
+        if ( o == null || getClass() != o.getClass() )
+            return false;
+
+        de.cookyapp.persistence.entities.RecipeOfTheMonthEntity that = (de.cookyapp.persistence.entities.RecipeOfTheMonthEntity) o;
+
+        if ( id != that.id )
+            return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * this.id;
+    }
+
+    private RecipeEntity recipe;
+
+    @ManyToOne( optional = false )
+    @JoinColumn( name = "RecipeID" )
+    public RecipeEntity getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe( RecipeEntity recipe ) {
+        this.recipe = recipe;
+    }
+
+    @Basic
+    @Column( name = "Updated On", nullable = true )
+    public LocalDate getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated( LocalDate updated ) {
+        this.updated = updated;
+    }
+
+
+}
+
+
+
+
+
