dc44a535d2d7d0b9a5449c10a54c0db4dbd21bac
Dominik Schaufelberger <dominik.schaufelberger@web.de>
2016-06-04 00:39:29 +0200

    COOK-161 - general work
    ADD: added a user friendly display name for the cookbook visibilities.
    ADD: cookbook panels that contain cookbooks are now expanded by default

--- CookyApp/src/main/java/de/cookyapp/enums/CookbookVisibility.java
+++ CookyApp/src/main/java/de/cookyapp/enums/CookbookVisibility.java
@@ -4,5 +4,15 @@ package de.cookyapp.enums;
  * Created by Dominik Schaufelberger on 27.11.2015.
  */
 public enum CookbookVisibility {
-    PRIVATE, FRIENDS, PUBLIC
+    PRIVATE( "Private" ), FRIENDS( "Shared" ), PUBLIC( "Public" );
+
+    private String displayName;
+
+    CookbookVisibility( String displayName ) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
