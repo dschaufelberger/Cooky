97ca47835b59467fcc937c221a82dd10e2a983d5
Dominik Schaufelberger <dominik.schaufelberger@web.de>
2016-06-04 14:43:11 +0200

    COOK-95 - implement uc: manage friends
    ADD: implemented friendship service

---
+++ CookyApp/src/main/java/de/cookyapp/service/services/FriendshipService.java
@@ -0,0 +1,119 @@
+package de.cookyapp.service.services;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import de.cookyapp.enums.FriendRequestState;
+import de.cookyapp.persistence.entities.FriendshipEntity;
+import de.cookyapp.persistence.entities.UserEntity;
+import de.cookyapp.persistence.repositories.app.IFriendshipRepository;
+import de.cookyapp.persistence.repositories.app.IUserCrudRepository;
+import de.cookyapp.service.dto.User;
+import de.cookyapp.service.exceptions.InvalidUserId;
+import de.cookyapp.service.services.interfaces.IFriendshipService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by Dominik Schaufelberger on 04.06.2016.
+ */
+@Service
+@Transactional
+public class FriendshipService implements IFriendshipService {
+    private IFriendshipRepository friendshipRepository;
+    private IUserCrudRepository userRepository;
+
+    @Autowired
+    public FriendshipService( IFriendshipRepository friendshipRepository, IUserCrudRepository userRepository ) {
+        this.friendshipRepository = friendshipRepository;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<User> getFriends( int forUser ) {
+        List<FriendshipEntity> requests = this.friendshipRepository.findByRequestedUserOrInquiringUserAndRequestState( forUser, forUser, FriendRequestState.ACCEPTED );
+        List<User> friends = new ArrayList<>( requests.size() );
+
+        for ( FriendshipEntity request : requests ) {
+            if ( request.getInquiringUser().getId() == forUser ) {
+                friends.add( new User( request.getRequestedUser() ) );
+            } else if ( request.getRequestedUser().getId() == forUser ) {
+                friends.add( new User( request.getInquiringUser() ) );
+            }
+        }
+
+        return friends;
+    }
+
+    @Override
+    public List<User> getIncomingFriendRequests( int requestedUser ) {
+        List<FriendshipEntity> requests = this.friendshipRepository.findByRequestedUserAndRequestState( requestedUser, FriendRequestState.PENDING );
+
+        return requests.stream()
+                .map( request -> new User( request.getInquiringUser() ) )
+                .collect( Collectors.toList() );
+    }
+
+    @Override
+    public List<User> getOutgoingFriendRequests( int inquiringUser ) {
+        List<FriendshipEntity> requests = this.friendshipRepository.findByInquiringUser( inquiringUser );
+
+        List<FriendshipEntity> rejected = new LinkedList<>();
+        List<FriendshipEntity> pending = new LinkedList<>();
+        for ( FriendshipEntity request : requests ) {
+            if ( request.getRequestState() == FriendRequestState.PENDING ) {
+                pending.add( request );
+            } else if ( request.getRequestState() == FriendRequestState.REJECTED ) {
+                rejected.add( request );
+            }
+        }
+
+        this.friendshipRepository.delete( rejected );
+
+        return pending.stream().map( request -> new User( request.getRequestedUser() ) ).collect( Collectors.toList() );
+    }
+
+    @Override
+    public void sendFriendRequest( int from, int to ) {
+        UserEntity inquiring = getUser( from );
+        UserEntity requested = getUser( to );
+
+        FriendshipEntity friendship = new FriendshipEntity();
+        friendship.setInquiringUser( inquiring );
+        friendship.setRequestedUser( requested );
+        friendship.setDate( LocalDateTime.now() );
+        friendship.setRequestState( FriendRequestState.PENDING );
+    }
+
+    @Override
+    public void acceptFriendRequest( int from, int to ) {
+        FriendshipEntity request = this.friendshipRepository.findByRequestedUserAndInquiringUserAndRequestState( from, to, FriendRequestState.PENDING );
+        if ( request != null ) {
+            request.setRequestState( FriendRequestState.ACCEPTED );
+            request.setDate( LocalDateTime.now() );
+            this.friendshipRepository.save( request );
+        }
+    }
+
+    @Override
+    public void rejectFriendRequest( int from, int to ) {
+        FriendshipEntity request = this.friendshipRepository.findByRequestedUserAndInquiringUserAndRequestState( from, to, FriendRequestState.PENDING );
+        if ( request != null ) {
+            request.setRequestState( FriendRequestState.REJECTED );
+            request.setDate( LocalDateTime.now() );
+            this.friendshipRepository.save( request );
+        }
+    }
+
+    private UserEntity getUser( int id ) {
+        UserEntity user = this.userRepository.findOne( id );
+        if ( user == null ) {
+            throw new InvalidUserId( id );
+        }
+        return user;
+    }
+}
---
+++ CookyApp/src/main/java/de/cookyapp/service/services/interfaces/IFriendshipService.java
@@ -0,0 +1,22 @@
+package de.cookyapp.service.services.interfaces;
+
+import java.util.List;
+
+import de.cookyapp.service.dto.User;
+
+/**
+ * Created by Dominik Schaufelberger on 04.06.2016.
+ */
+public interface IFriendshipService {
+    List<User> getFriends( int forUser );
+
+    List<User> getIncomingFriendRequests( int requestedUser );
+
+    List<User> getOutgoingFriendRequests( int inquiringUser );
+
+    void sendFriendRequest( int from, int to );
+
+    void acceptFriendRequest( int from, int to );
+
+    void rejectFriendRequest( int from, int to );
+}
